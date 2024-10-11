ef4e94ca27625308f594ff62199da149e4117ed6
said4ik <muxammadaminartikov@gmail.com>
2024-10-11 11:17:17 +0500

    bot

--- src/main/java/org/example/metube/YouTubeDownloaderBot.java
+++ src/main/java/org/example/metube/YouTubeDownloaderBot.java
@@ -1,21 +1,29 @@
 package org.example.metube;
 
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 
 @Component
-public class YouTubeDownloaderBot extends MyBot {
+public class YouTubeDownloaderBot extends TelegramLongPollingBot {
 
-    @Autowired
-    private  S3Uploader s3Uploader ;
+    public YouTubeDownloaderBot() {
+        super("7716409596:AAFzFhIbMs_8JFpRNMXuHQL5OFg6ZQBN0Hs");
+    }
 
 
-    public void onUpdate(Update update) {
+    @Override
+    public String getBotUsername() {
+        return "MetubeMediaBot";
+    }
+
+
+
+    @Override
+    public void onUpdateReceived(Update update) {
         if (update.hasMessage() && update.getMessage().hasText()) {
             String messageText = update.getMessage().getText();
             Long chatId = update.getMessage().getChatId();
@@ -25,15 +33,10 @@ public class YouTubeDownloaderBot extends MyBot {
                 return;
             }
 
-            try {
-                String uploadedUrl = s3Uploader.uploadVideoToS3(messageText);
-                sendMessage(chatId, "Video yuklandi: " + uploadedUrl);
-            } catch (Exception e) {
-                sendMessage(chatId, "Xato: " + e.getMessage());
-            }
-        }
-    }
 
+        }
+
+    }
 
     private void sendMessage(Long chatId, String text) {
         try {
@@ -42,6 +45,4 @@ public class YouTubeDownloaderBot extends MyBot {
             e.printStackTrace();
         }
     }
-
-
 }
